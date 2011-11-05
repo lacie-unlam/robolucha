692ed8c553d686a4cdecc0f001cb8033acc746cf
Cristian Rasch <cristianrasch@gmail.com>
2011-11-05 11:17:57 -0300

    Adding cap rake:invoke deployment task

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,3 +27,11 @@ namespace :deploy do
   end
 end
 after 'deploy', 'deploy:cleanup'
+
+namespace :rake do  
+  desc 'Run a task on a remote server.'  
+  # run like: cap rake:invoke task=a_certain_task  
+  task :invoke do  
+    run "cd #{current_release} && bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}"  
+  end  
+end
